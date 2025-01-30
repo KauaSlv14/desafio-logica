22613648b441067f809be84a37e553308149c60a
KauaSlv14 <zka18952@gmail.com>
2025-01-30 16:49:13 -0300

    desafio

--- github.py
+++ github.py
@@ -1,1 +1,19 @@
-print("Eu sou o the best")
+nome=input("digite o nome do herói: ")
+xp=int(input("Digite o nivel do heroi")) 
+if xp <1000: 
+  nivel=  ("ferro")
+elif xp>=1001 and xp <=2000:
+  nivel= ("Bronze") 
+elif xp >=2001 and xp <=5000:
+   nivel=("Prata") 
+elif xp >=50001 and xp <=7000: 
+  nivel=("Ouro") 
+elif xp >= 7001 and xp <=8000:
+   nivel=("platina") 
+elif xp>= 8001 and xp <=9000: 
+  nivel =("Ascendente") 
+elif xp >=9001 and xp >=1000:
+    print ("imortal") 
+else:
+    nivel =print("Radiante")
+print("O heroi de nome", nome,"esta mo nível de ", nivel) 
